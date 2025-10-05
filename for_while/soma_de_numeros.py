b6668f84332fd3c416ebf7f6065f808521988331
Fernando Oliveira <fernandooliveira@MacBook-Air-de-Fernando.local>
2025-10-05 18:23:10 -0300

    soma de numeros

---
+++ for_while/soma_de_numeros.py
@@ -0,0 +1,10 @@
+def main():
+
+    total = 0
+    valores = [10, 20, 30, 40, 50]
+    for valor in valores:
+        total += valor
+    print(f'A soma total das receitas é: {total}')
+
+if __name__ == '__main__':
+    main()
