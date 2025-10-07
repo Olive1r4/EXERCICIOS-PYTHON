7d718dc7472f85d163269fe16cba24e8f7d05bdb
Fernando Freitas de Oliveira <ffoliveira@banestes.com.br>
2025-10-07 09:12:48 -0300

    Saudação personalizada

---
+++ funcoes/saudacao_personalizada.py
@@ -0,0 +1,14 @@
+def saudacao(hora):
+    if (hora < 12):
+        print('Bom dia!')
+    elif (hora >= 12 and hora < 18):
+        print('Boa tarde!')
+    else:
+        print('Boa noite!')
+
+def main():
+    hora = int(input('Digite a hora atual (0-23): '))
+    saudacao(hora)
+
+if __name__ == '__main__':
+    main()
